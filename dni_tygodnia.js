87518d32e2688bece1156c6d4e182e6bf1ccb1ec
NoMercyPL <40893134+NoMercyPL@users.noreply.github.com>
2020-05-09 04:34:04 +0200

    Update and rename comp.js to dni_tygodnia.js

---
+++ dni_tygodnia.js
@@ -0,0 +1,28 @@
+<SCRIPT LANGUAGE="JavaScript">
+<!-- 
+document.write("<center><b>")
+now = new Date()
+if (now.getDay() == 5)
+document.write("Cudownie, już piątek!")
+
+if (now.getDay() == 6)
+document.write("Wreszcie upragniony weekend!")
+
+if (now.getDay() == 0)
+document.write("Ojej, weekend się powoli kończy!")
+
+if (now.getDay() == 1)
+document.write("Dzisiaj jest poniedziałek. To na pewno będzie udany tydzień!")
+
+if (now.getDay() == 2)
+document.write("Wtorek, poniedziałek już na szczęście za nami!")
+
+if (now.getDay() == 3)
+document.write("Środa, czy Twój tydzień jest również udany?")
+
+if (now.getDay() == 4)
+document.write("Czwartek, weekend już tuż tuż...")
+document.write("</b></center>")
+
+//-->
+</SCRIPT>
